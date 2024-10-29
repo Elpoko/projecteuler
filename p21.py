cf430953270e112f4c1d548b6a9daba1804575a8
Elpoko <tommccarthyprojects@gmail.com>
2024-10-29 18:28:54 +0000

    add p21

---
+++ p21.py
@@ -0,0 +1,7 @@
+# from functools import reduce
+# a = reduce(lambda x, y: x + y, [int(i) for i in str(reduce(lambda x, y: x * y, range(1, 100)))])
+# print(a)
+
+# a = reduce(lambda x, y: x + y, ["h", "e", "l", "l", "o"])
+# print(a)
+
